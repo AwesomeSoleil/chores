eec2c88bba9fe710c78656dac2888fd08de6fc8c
Dmitriy Startsev <startsevd@gmail.com>
2018-04-14 23:20:26 +0300

    ejected empty project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './index.css';
+
+ReactDOM.render(
+    <div className={styles.index}>app</div>,
+    document.querySelector('#root')
+);
